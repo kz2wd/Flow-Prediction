ae624b77cdfd75ce8cc06b4aefd03720498af346
kz2wd <yrus.chan@gmail.com>
2025-04-21 10:37:14 +0900

    progress on training

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import matplotlib.pyplot as plt
-import numpy as np
 
 from space_exploration.data_viz import Plotter
 from space_exploration.models.A.A03 import A03
 from space_exploration.models.C.C04 import C04
 from space_exploration.models.C.C05 import C05
 from space_exploration.models.C.C06 import C06
-from space_exploration.models.C.CBase import CBase
 
 
 def test():
     model = C04()
     # model.train(1, 1, 4)
+    # model.generate_pipeline_training()
+    # model.generate_datasets("test", 1)
     model.test(20)
-    model.benchmark()
-    Plotter.plot_mse(model, "mse")
+    # model.benchmark()
+    # Plotter.plot_mse(model, "mse")
 
 def test_multiple():
     models = [C04(), C05(), C06()]
@@ -22,6 +21,5 @@ def test_multiple():
         model.test(5)
 
 if __name__ == '__main__':
-    # compare_data()
     # test_multiple()
     test()
--- space_exploration/models/GAN3D.py
+++ space_exploration/models/GAN3D.py
@@ -1,20 +1,16 @@
 import os
 import re
-import sys
 import time
 from abc import ABC, abstractmethod
-from plistlib import loads
 
 import h5py
 import mlflow
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
-from sklearn.metrics import mean_squared_error
 
 from FolderManager import FolderManager
 from space_exploration.simulation_channel import SimulationChannel
-
 
 
 class GAN3D(ABC):
@@ -58,7 +54,6 @@ class GAN3D(ABC):
     def build(self):
         if self.already_built == True:
             return
-
         self.already_built = True
         self._generator = self.generator()
         self._generator_loss = self.generator_loss()
@@ -75,26 +70,51 @@ class GAN3D(ABC):
         )
 
     def generate_datasets(self, target_folder, batch_size, train_split=0.7, test_split=0.15, validation_split=0.15):
-        records = []
         records_folder = FolderManager.tfrecords / target_folder
-        for file in os.listdir(records_folder):
-            res = re.search(".tfrecord", file)
-            if res:
-                records.append(records_folder / file)
-        raw_dataset = tf.data.TFRecordDataset(records)
-        parsed_dataset = raw_dataset.map(self.channel.tf_parser)
-        DATASET_SIZE = tf.data.experimental.cardinality(parsed_dataset).numpy()
+        record_files = [
+            str(records_folder / file)
+            for file in os.listdir(records_folder)
+            if file.endswith(".tfrecords")
+        ]
+
+        # Read and parse dataset
+        raw_dataset = tf.data.Dataset.list_files(record_files)
+        split_dataset = raw_dataset.interleave(lambda x: tf.data.TFRecordDataset(x).take(30),
+                                               cycle_length=16,
+                                               num_parallel_calls=tf.data.AUTOTUNE)
+        parsed_dataset = split_dataset.map(lambda x: self.channel.tf_parser(x), num_parallel_calls=tf.data.AUTOTUNE)
+
+        # Filter out problematic samples (e.g., empty ones)
+        # def is_valid(x, y=None):
+        #     if y is None:
+        #         return tf.size(x) > 0
+        #     return tf.logical_and(tf.size(x) > 0, tf.size(y) > 0)
+        #
+        # # Apply filtering
+        # parsed_dataset = parsed_dataset.filter(is_valid)
+
+        # Shuffle and batch before splitting
+        parsed_dataset = parsed_dataset.shuffle(10000)
+        parsed_dataset = parsed_dataset.batch(batch_size, drop_remainder=True)
+
+        for example in parsed_dataset.take(1):
+            print("Example:", example)
+
+        input()
+
+        return parsed_dataset
+        # Compute dataset sizes
+        DATASET_SIZE = 100
         train_size = int(train_split * DATASET_SIZE)
-        val_size = int(validation_split * DATASET_SIZE)
         test_size = int(test_split * DATASET_SIZE)
-        parsed_dataset.shuffle(buffer_size=10000)
+        val_size = DATASET_SIZE - train_size - test_size  # to avoid rounding issues
+
+        # Split
         train_dataset = parsed_dataset.take(train_size)
-        train_dataset = train_dataset.batch(batch_size=batch_size)
-        test_dataset = parsed_dataset.skip(train_size)
-        val_dataset = test_dataset.skip(test_size)
-        val_dataset = val_dataset.batch(batch_size=batch_size)
-        test_dataset = test_dataset.take(test_size)
-        test_dataset = test_dataset.batch(batch_size=batch_size)
+        test_val = parsed_dataset.skip(train_size)
+        val_dataset = test_val.take(val_size)
+        test_dataset = test_val.skip(val_size)
+
         return train_dataset, val_dataset, test_dataset
 
     def test(self, test_sample_amount=50):
@@ -111,10 +131,15 @@ class GAN3D(ABC):
 
         # train & test folder are inversed bcause of the amount of data they contained,
         # train folder is 1/10th of test size, there is a good reason explained in the readme of original code
-        dataset_full, _, _ = self.generate_datasets("train", batch_size=4, train_split=1.0)
+        dataset_full, _, _ = self.generate_datasets("train", batch_size=1, train_split=1.0)
 
         self.build()
-        self.checkpoint.restore(tf.train.latest_checkpoint(FolderManager.checkpoints(self)))
+        latest_ckpt = tf.train.latest_checkpoint(FolderManager.checkpoints(self))
+        print(f"Found checkpoint: {latest_ckpt}.")
+        status = self.checkpoint.restore(latest_ckpt)
+
+        # IF MESSY RESULTS OBTAINED, TRY TO ASSERT CONSUMED !! (it will yield interesting errors :)
+        # status.assert_consumed()
 
         # samples, stream wise resolution, wall normal wise resolution, span wise resolution, velocities.
         # velocities -> 0, u, stream wise | 1, v, wall normal wise | 2, w, spawn wise
@@ -144,6 +169,103 @@ class GAN3D(ABC):
         mse = np.mean((self.y_target_normalized - self.y_predict_normalized) ** 2)
         print(mse)
         print(f'Predict mean: {np.mean(self.y_predict_normalized)}, std: {np.std(self.y_predict_normalized)}')
+
+    def generate_pipeline_training(self, validation_split=1, shuffle_buffer=200, batch_size=4,
+                                   n_prefetch=4):
+
+        tfr_path = FolderManager.tfrecords / "test"
+        tfr_files = sorted(
+            [os.path.join(tfr_path, f) for f in os.listdir(tfr_path) if os.path.isfile(os.path.join(tfr_path, f))])
+        regex = re.compile(f'.tfrecords')
+        tfr_files = ([string for string in tfr_files if re.search(regex, string)])
+        n_samples_per_tfr = np.array([int(s.split('.')[-2][-3:]) for s in tfr_files])
+        n_samples_per_tfr = n_samples_per_tfr[np.argsort(-n_samples_per_tfr)]
+        cumulative_samples_per_tfr = np.cumsum(np.array(n_samples_per_tfr))
+        tot_samples_per_ds = sum(n_samples_per_tfr)
+        # n_tfr_loaded_per_ds = int(tfr_files[0].split('_')[-3][-3:])
+        n_tfr_loaded_per_ds = 119  # some were corrupted
+        # n_tfr_loaded_per_ds = int(tfr_files[0].split('_')[-3][-4:])
+        tfr_files = [string for string in tfr_files if int(string.split('_')[-3][:3]) <= n_tfr_loaded_per_ds]
+        # tfr_files = [string for string in tfr_files if int(string.split('_')[-3][:4]) <= n_tfr_loaded_per_ds]
+
+        n_samp_train = int(sum(n_samples_per_tfr) * (1 - validation_split))
+        n_samp_valid = sum(n_samples_per_tfr) - n_samp_train
+
+        (n_files_train, samples_train_left) = np.divmod(n_samp_train, n_samples_per_tfr[0])
+
+        if samples_train_left > 0:
+            n_files_train += 1
+
+        tfr_files_train = [string for string in tfr_files if int(string.split('_')[-3][:3]) <= n_files_train]
+        # tfr_files_train = [string for string in tfr_files if int(string.split('_')[-3][:4]) <= n_files_train]
+        n_tfr_left = np.sum(np.where(cumulative_samples_per_tfr < samples_train_left, 1, 0)) + 1
+
+        if sum([int(s.split('.')[-2][-2:]) for s in tfr_files_train]) != n_samp_train:
+
+            shared_tfr = tfr_files_train[-1]
+            tfr_files_valid = [shared_tfr]
+        else:
+
+            shared_tfr = ''
+            tfr_files_valid = list()
+
+        tfr_files_valid.extend([string for string in tfr_files if string not in tfr_files_train])
+        tfr_files_valid = sorted(tfr_files_valid)
+        shared_tfr_out = tf.constant(shared_tfr)
+        n_tfr_per_ds = tf.constant(n_tfr_loaded_per_ds)
+        n_samples_loaded_per_tfr = list()
+
+        if n_tfr_loaded_per_ds > 1:
+
+            n_samples_loaded_per_tfr.extend(n_samples_per_tfr[:n_tfr_loaded_per_ds - 1])
+            n_samples_loaded_per_tfr.append(tot_samples_per_ds - cumulative_samples_per_tfr[n_tfr_loaded_per_ds - 2])
+
+        else:
+
+            n_samples_loaded_per_tfr.append(tot_samples_per_ds)
+
+        n_samples_loaded_per_tfr = np.array(n_samples_loaded_per_tfr)
+        # tfr_files_train_ds = tf.data.Dataset.list_files(tfr_files_train, seed=666)
+        tfr_files_val_ds = tf.data.Dataset.list_files(tfr_files_valid, seed=686)
+        if n_tfr_left > 1:
+
+            samples_train_shared = samples_train_left - cumulative_samples_per_tfr[n_tfr_left - 2]
+            n_samples_tfr_shared = n_samples_loaded_per_tfr[n_tfr_left - 1]
+
+        else:
+
+            samples_train_shared = samples_train_left
+            n_samples_tfr_shared = n_samples_loaded_per_tfr[0]
+
+        # tfr_files_train_ds = tfr_files_train_ds.interleave(
+        #    lambda x : tf.data.TFRecordDataset(x).take(samples_train_shared) if tf.math.equal(x, shared_tfr_out) else tf.data.TFRecordDataset(x).take(tf.gather(n_samples_loaded_per_tfr, tf.strings.to_number(tf.strings.split(tf.strings.split(x, sep='_')[-3],sep='-')[0], tf.int32)-1)),
+        #    cycle_length=16,
+        #    num_parallel_calls=tf.data.experimental.AUTOTUNE
+        # )
+
+        tfr_files_val_ds = tfr_files_val_ds.interleave(
+            lambda x: tf.data.TFRecordDataset(x).skip(samples_train_shared).take(
+                n_samples_tfr_shared - samples_train_shared) if tf.math.equal(x,
+                                                                              shared_tfr_out) else tf.data.TFRecordDataset(
+                x).take(tf.gather(n_samples_loaded_per_tfr,
+                                  tf.strings.to_number(tf.strings.split(tf.strings.split(x, sep='_')[-3], sep='-')[0],
+                                                       tf.int32) - 1)),
+            cycle_length=16,
+            num_parallel_calls=tf.data.experimental.AUTOTUNE
+        )
+
+        # dataset_train = tfr_files_train_ds.map(lambda x: tf_parser(x, root_folder), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        # dataset_train = dataset_train.shuffle(shuffle_buffer)
+        # dataset_train = dataset_train.batch(batch_size=batch_size)
+        # dataset_train = dataset_train.prefetch(n_prefetch)
+
+        dataset_valid = tfr_files_val_ds.map(lambda x: self.tf_parser(x),
+                                             num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        # dataset_valid = dataset_valid.shuffle(shuffle_buffer)
+        dataset_valid = dataset_valid.batch(batch_size=batch_size)
+        dataset_valid = dataset_valid.prefetch(n_prefetch)
+
+        return dataset_valid  # dataset_train, dataset_valid
 
     def save(self):
         save_dataset = FolderManager.predictions_file(self)
@@ -178,7 +300,6 @@ class GAN3D(ABC):
             f.create_dataset('u_mse_y_wise', data=u_mse, compression='gzip')
             f.create_dataset('v_mse_y_wise', data=v_mse, compression='gzip')
             f.create_dataset('w_mse_y_wise', data=w_mse, compression='gzip')
-
 
     def train(self, epochs, saving_freq, batch_size):
         @tf.function
