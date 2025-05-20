68d54fe92bed3e525dab7197c4cbd2a4374d8739
kz2wd <yrus.chan@gmail.com>
2025-05-20 19:04:48 +0900

    end of the day

--- space_exploration/data_export/simu_analysis.py
+++ space_exploration/data_export/simu_analysis.py
@@ -1,5 +1,11 @@
+import dask.array as da
+
 import s3_access
 
 if __name__ == "__main__":
     fs = s3_access.fs
-    print(fs.ls("simulations"))
+    target_bucket = 'simulations'
+    target_dataset = 'paper-dataset.zarr'
+
+    ds = da.from_zarr(f"s3://{target_bucket}/{target_dataset}")
+    print(ds)
