database handling progress + fixed paper dataset normalization & shape!!

scripts/export_paper_metadata.py
@@ -10,18 +10,18 @@ from pathlib import Path
 from sqlalchemy import create_engine
 
 
-def add_dataset(session, name, s3_storage_name, raw_data_normalized, y_dimensions, u_means,
+def add_dataset(session, name, s3_storage_name, scaling, y_dimensions, u_means,
                 v_means, w_means, u_stds, v_stds, w_stds):
     dataset = Dataset(
         name=name,
         s3_storage_name=s3_storage_name,
-        raw_data_normalized=raw_data_normalized,
+        scaling=scaling,
     )
 
-    for i in range(len(y_dimensions) - 1):
+    for i in range(64):
         stat = DatasetStat(
             y_index=i,
-            y_coord=float(y_dimension[i]),
+            y_coord=float(y_dimensions[i]),
             u_mean=float(u_means[i]),
             v_mean=float(v_means[i]),
             w_mean=float(w_means[i]),
@@ -46,7 +46,7 @@ if __name__ == "__main__":
 
     channel_data_file: Path = FolderManager.tfrecords / "scaling.npz"
     channel_data = ChannelData(channel_data_file)
-    y_dimension = np.load(FolderManager.channel_coordinates / "coordY.npy")
+    y_dimensions = np.load(FolderManager.channel_coordinates / "coordY.npy")
 
     u_means = channel_data.U_mean.reshape(-1)
     v_means = channel_data.V_mean.reshape(-1)
@@ -60,8 +60,8 @@ if __name__ == "__main__":
         session=session,
         name="paper-validation",
         s3_storage_name="paper-dataset.zarr",
-        raw_data_normalized=True,
-        y_dimensions=y_dimension,
+        scaling=100 / 3,
+        y_dimensions=y_dimensions,
         u_means=u_means,
         v_means=v_means,
         w_means=w_means,

scripts/simu_analysis.py
@@ -7,21 +7,15 @@ from space_exploration.dataset import s3_access
 
 def plot_y_velo(ds, plot_name):
 
-    if ds.shape[-1] == 3:
-        ds = da.linalg.norm(ds, axis=4)
-        velocity_mean = ds.mean(axis=(0, 1, 3)).compute()
-    else:
-        ds = da.linalg.norm(ds, axis=1)
-        print(ds.shape)
-        velocity_mean = ds.mean(axis=(0, 1, 3)).compute()
+
+    velocity_mean = ds[:, 0].mean(axis=(0, 1, 3)).compute()
+
 
     plt.semilogx(velocity_mean, label="mean velocity")
-    plt.title("velocity mean along wall-normal")
+    plt.title("mean stream wise velocity along wall-normal")
     plt.legend()
     plt.grid(True)
     plt.savefig(plot_name)
-
-
 
 
 if __name__ == "__main__":

space_exploration/dataset/s3_dataset.py
@@ -1,19 +1,17 @@
 import torch
 from torch.utils.data import Dataset
-import h5py
+
 
 from space_exploration.simulation_channel.SimulationChannel import SimulationChannel
 
 
-class HDF5Dataset(Dataset):
+class S3Dataset(Dataset):
     def __init__(self, ds, channel: SimulationChannel):
 
         self.ds = ds
 
-        self.x = self.file['x'][:amount, ...]  # [X, x, 1, z, 3]
-        self.y = self.file['y'][:amount, :, :channel.prediction_sub_space.y[1], :, :]  # [X, x, y, z, 3]
-
-
+        self.x = self.ds[..., :, :, 1, :]  # [B, 3, x, 1, z]
+        self.y = self.ds[..., :, :, channel.prediction_sub_space.y[1], :]  # [B, 3, x, y, z]
 
     def __len__(self):
         return len(self.x)