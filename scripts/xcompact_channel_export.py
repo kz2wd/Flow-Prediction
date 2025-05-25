b786afad94798b03b39deb18b8a33d927f7aecdb
kz2wd <yrus.chan@gmail.com>
2025-05-25 15:02:29 +0900

    Addec channel export file

--- scripts/xcompact_channel_export.py
+++ scripts/xcompact_channel_export.py
@@ -1,29 +1,65 @@
 import argparse
-
-import numpy as np
+from pathlib import Path
 
 from scripts.database_add import add_channel
-from space_exploration.FolderManager import FolderManager
-from space_exploration.beans.channel_bean import Channel
-from space_exploration.beans.channel_y_bean import ChannelY
+from scripts.parser_utils import dir_path
 from space_exploration.dataset import db_access
+
+
+def read_ypi(folder):
+    with open(folder / "ypi.dat", 'r') as f:
+        return [float(line.strip()) for line in f.readlines()]
+
+def get_channel_data(filepath):
+    channel_data = {}
+    with open(filepath, 'r') as file:
+        for line in file:
+            # Remove everything after '!' (comments)
+            line = line.split('!')[0].strip()
+            if '=' in line:
+                key, value = line.split('=', 1)
+                key = key.strip()
+                value = value.strip()
+                # Try converting value to int, float, or keep as string
+                try:
+                    value = int(value)
+                except ValueError:
+                    try:
+                        value = float(value)
+                    except ValueError:
+                        pass
+                channel_data[key] = value
+    return channel_data
 
 
 if __name__ == '__main__':
     session = db_access.get_session()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("simulation_path", metavar="simulation-path", required=True, type=dir_path)
+    parser.add_argument("--simulation-path", required=True, type=dir_path)
+    parser.add_argument("--channel-name", required=True, type=str)
+    parser.add_argument("--channel-scale", required=True, type=float)
+    parser.add_argument("--input-file", type=str)
 
     args = parser.parse_args()
-    # Todo
 
-    y_dim = np.load(FolderManager.channel_coordinates / "coordY.npy")
+    simulation_folder = Path(args.simulation_path)
+
+    if args.input_file:
+        input_file = Path(args.input_file)
+    else:
+        input_file = simulation_folder / "input.i3d"
+
+    channel_data = get_channel_data(input_file)
+    y_dim = read_ypi(simulation_folder)
+
     add_channel(session,
-                "paper-channel",
-                64, np.pi,
+                args.channel_name,
+                channel_data['nx'], channel_data['xlx'],
                 y_dim,
-                64, np.pi / 2,
-                200)
+                channel_data['nz'], channel_data['zlz'],
+                args.channel_scale)
 
     session.commit()
+
+    print(f"Added channel [{args.channel_name}].")
