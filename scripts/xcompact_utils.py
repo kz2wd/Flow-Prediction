87baae7bb523b904b049696a6c86ca3b38776f04
kz2wd <yrus.chan@gmail.com>
2025-06-15 01:17:23 +0900

    [ADDED] X & Y export

--- scripts/xcompact_utils.py
+++ scripts/xcompact_utils.py
@@ -24,7 +24,7 @@ def get_shape_from_xdmf(folder, snapshot_index):
     raise ValueError("Could not find grid dimensions in XDMF.")
 
 
-def load_snapshot(snapshot_index, dims, folder, mu_viscosity, y_0_1_distance):
+def load_snapshot(snapshot_index, dims, folder, mu_viscosity, y_0_1_distance, y_lim=None):
     nx, ny, nz = dims[::-1]  # because dims = (nz, ny, nx)
     shape = (nx, ny, nz)
     components = []
@@ -35,6 +35,9 @@ def load_snapshot(snapshot_index, dims, folder, mu_viscosity, y_0_1_distance):
         components.append(data)
     y_sample = np.stack(components, axis=0)  # Shape: [3, nx, ny, nz]
 
+    if y_lim is not None:
+        y_sample = y_sample[:, :, :y_lim, :]
+
     pressure_file = folder / f"pp-{snapshot_index}.bin"
     pressure_data = np.fromfile(pressure_file, dtype=np.float64).reshape(shape, order='F')
     pressure_data = np.float32(pressure_data)[:, 1, :]
@@ -42,7 +45,11 @@ def load_snapshot(snapshot_index, dims, folder, mu_viscosity, y_0_1_distance):
     tau_x = mu_viscosity * (y_sample[0, :, 1, :] - y_sample[0, :, 0, :]) / y_0_1_distance
     tau_z = mu_viscosity * (y_sample[2, :, 1, :] - y_sample[2, :, 0, :]) / y_0_1_distance
 
-    x_sample = np.stack([pressure_data, tau_x, tau_z], axis=0)
+    x_sample = np.stack([
+        pressure_data[:, np.newaxis, :],
+        tau_x[:, np.newaxis, :],
+        tau_z[:, np.newaxis, :]
+    ], axis=0)  # Final shape: (3, nx, 1, nz)
 
     return x_sample, y_sample
 
@@ -56,7 +63,7 @@ def get_ids(folder: Path):
             matching_ids.add(file_id)
     return sorted(list(matching_ids))
 
-def get_snapshot_xy(simulation_folder: Path, input_file_path=None):
+def get_snapshot_xy(simulation_folder: Path, input_file_path=None, y_lim=None):
     folder = simulation_folder / "data"
     indices = get_ids(folder)
     dims = get_shape_from_xdmf(folder, indices[0])
@@ -74,11 +81,9 @@ def get_snapshot_xy(simulation_folder: Path, input_file_path=None):
     y_das = []
     x_das = []
     for idx in tqdm.tqdm(indices):
-        delayed_yx = delayed(load_snapshot)(idx, dims, folder, mu_viscosity, y_0_1_distance)
-
+        delayed_yx = delayed(load_snapshot)(idx, dims, folder, mu_viscosity, y_0_1_distance, y_lim)
         x_da = da.from_delayed(delayed_yx[0], shape=(3, nx, 1, nz), dtype=np.float32)
-        y_da = da.from_delayed(delayed_yx[1], shape=(3, nx, ny, nz), dtype=np.float32)
-
+        y_da = da.from_delayed(delayed_yx[1], shape=(3, nx, y_lim, nz), dtype=np.float32)
         y_das.append(y_da)
         x_das.append(x_da)
 
@@ -86,6 +91,14 @@ def get_snapshot_xy(simulation_folder: Path, input_file_path=None):
     x = da.stack(x_das, axis=0)  # Shape: [N, 3, nx, 1,  nz]
     return x, y
 
+
+def get_chunk_size(inner_shape, target_chunk_MB = 200):
+    dtype = np.float32
+    bytes_per_sample = np.prod(inner_shape) * np.dtype(dtype).itemsize
+    target_chunk_bytes = target_chunk_MB * 1024 ** 2
+    samples_per_chunk = target_chunk_bytes // bytes_per_sample
+
+    return samples_per_chunk, *inner_shape
 
 def build_export_metadata(session, ds, dataset_name, scaling, channel):
 
