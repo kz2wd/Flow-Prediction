89777f00e387f941b441a29165ad10abc63be23e
kz2wd <yrus.chan@gmail.com>
2025-06-20 18:09:18 +0900

    * setup main for training

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from space_exploration.training import ModelTraining
+from space_exploration.training.training import ModelTraining
 
 
 
 def test():
-    training = ModelTraining("A", "re200-sr005etot", "COMPONENT_NORMALIZE", "Y_ALONG_COMPONENT_NORMALIZE", 4, 500)
+    training = ModelTraining("A", "re200-sr05etot", "COMPONENT_NORMALIZE", "Y_ALONG_COMPONENT_NORMALIZE", 4)
     training.run()
 
 
