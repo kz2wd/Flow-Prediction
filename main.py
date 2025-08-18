22074c602f95583a46b3be1977ea2692c9c07339
kz2wd <yrus.chan@gmail.com>
2025-08-18 23:36:40 +0900

    [FIXED] some issue with benchmark but not perfect

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from space_exploration.training.training import ModelTraining
 
 
 def test():
-    training = ModelTraining("D-test", "re200-sr05etot", "SCALE_FILTERING_RENORMALIZED",
-                             "Y_ALONG_COMPONENT_NORMALIZE", 4, name="end-channel-scale_filtering", data_amount=500)
+    training = ModelTraining("D", "re200-sr05etot", "SCALE_FILTERING_RENORMALIZED",
+                             "Y_ALONG_COMPONENT_NORMALIZE", 4, name="end-channel-scale_filtering")
     training.run()
 
 
