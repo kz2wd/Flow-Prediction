f3771351022857ef850cca0338e6722cf394c75a
kz2wd <yrus.chan@gmail.com>
2025-08-29 15:58:53 +0900

    Tweaked training func

--- space_exploration/models/GAN3D.py
+++ space_exploration/models/GAN3D.py
@@ -90,8 +90,7 @@ class GAN3D(PredictionModel):
 
             # === Generator forward and loss ===
             y_pred = self.generator(x_target)
-            with torch.no_grad():
-                fake_output_for_gen = self.discriminator(y_pred)
+            fake_output_for_gen = self.discriminator(y_pred)
             gen_loss = self.generator_loss(fake_output_for_gen, y_pred, y_target)
 
             self.generator_optimizer.zero_grad()
